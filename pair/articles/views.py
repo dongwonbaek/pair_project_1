700bf3881f6f8a813149f5c4d30a4ae003a78c81
suyoung049 <suyoung049@gmail.com>
2022-09-30 12:55:15 +0900

    오전 실습

--- pair/articles/views.py
+++ pair/articles/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Review
 
 # Create your views here.
+
+
+def index(request):
+    review = Review.objects.all()
+
+    context = {
+        "reviews": review,
+    }
+
+    return render(request, "articles/index.html", context)
+
+
+def create(request):
+    return render(request, "articles/create.html")
+
+
+def write(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    Review.objects.create(title=title, content=content)
+
+    return redirect("articles:index")
+
+
+def detail(request, pk):
+    review = Review.objects.get(id=pk)
+
+    context = {
+        "review": review,
+    }
+    return render(request, "articles/detail.html", context)
+
+
+def delete(request, pk):
+    Review.objects.get(id=pk).delete()
+
+    return redirect("articles:index")
+
+
+def edit(request, pk):
+    review = Review.objects.get(id=pk)
+
+    context = {
+        "review": review,
+    }
+    return render(request, "articles/edit.html", context)
+
+
+def update(request, pk):
+    review = Review.objects.get(id=pk)
+
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    review.title = title
+    review.content = content
+
+    review.save()
+
+    return redirect("articles:detail", review.pk)
